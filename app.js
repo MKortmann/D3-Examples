0d8d0751be042f39522f5e9b2fceea67523a1b9b
MKortmann <companymvms@gmail.com>
2020-05-04 21:50:55 +0200

    add: text

--- app.js
+++ app.js
@@ -16,14 +16,33 @@ const svg = canvas.append("svg").attr("width", "800").attr("height", "250");
 
 svg
   .append("circle")
-  .attr("cx", "50")
-  .attr("cy", "50")
+  .attr("cx", 50)
+  .attr("cy", 50)
   .attr("r", 50)
   .attr("fill", "red");
 
 svg
+  .append("line")
+  .attr("x1", 0)
+  .attr("x2", 100)
+  .attr("y1", 80)
+  .attr("y2", 20)
+  .attr("stroke", "black");
+
+svg
   .append("rect")
-  .attr("x", "100")
-  .attr("y", "100")
-  .attr("width", "50")
-  .attr("height", "80");
+  .attr("x", 50)
+  .attr("y", 50)
+  .attr("width", 50)
+  .attr("height", 80)
+  .attr("rx", 15)
+  .attr("ry", 15)
+  .attr("fill", "green");
+
+svg
+  .append("text")
+  .text("Hello There")
+  .attr("fill", "blue")
+  .attr("font-size", "28")
+  .attr("x", 50)
+  .attr("y", 120);
