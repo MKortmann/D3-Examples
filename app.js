e7893d31aa96823e5c461b000e2673ad16d2bb17
MKortmann <companymvms@gmail.com>
2020-05-05 08:40:03 +0200

    add: hardcode barchart

--- app.js
+++ app.js
@@ -11,38 +11,83 @@
 // canva will be a d3 wrap object
 const canvas = d3.select(".canva");
 
+let dataArray = [4, 15, 34];
+
 // add an svg element
-const svg = canvas.append("svg").attr("width", "800").attr("height", "250");
+// const svg = canvas.append("svg").attr("width", "800").attr("height", "600");
 
-svg
-  .append("circle")
-  .attr("cx", 50)
-  .attr("cy", 50)
-  .attr("r", 50)
-  .attr("fill", "red");
+const svg = canvas.select("svg");
 
-svg
-  .append("line")
-  .attr("x1", 0)
-  .attr("x2", 100)
-  .attr("y1", 80)
-  .attr("y2", 20)
-  .attr("stroke", "black");
+// const rect = svg.append("rect");
 
-svg
-  .append("rect")
-  .attr("x", 50)
-  .attr("y", 50)
-  .attr("width", 50)
-  .attr("height", 80)
-  .attr("rx", 15)
-  .attr("ry", 15)
-  .attr("fill", "green");
+const rect = svg.selectAll("rect");
 
-svg
-  .append("text")
-  .text("Hello There")
-  .attr("fill", "blue")
-  .attr("font-size", "28")
-  .attr("x", 50)
-  .attr("y", 120);
+rect
+  .attr("width", 24)
+  .data(dataArray)
+  .attr("height", function (d) {
+    return d * 3;
+  })
+  .attr("fill", "orange")
+  .attr("y", function (d, i) {
+    console.log(d, i);
+    return 100 - d * 3;
+  })
+  .attr("x", function (d, i) {
+    console.log(d, i);
+    return i * 25;
+  });
+
+console.log(rect);
+
+// svg
+//   .append("circle")
+//   .attr("cx", 50)
+//   .attr("cy", 50)
+//   .attr("r", 50)
+//   .attr("fill", "red");
+
+// svg
+//   .append("line")
+//   .attr("x1", 0)
+//   .attr("x2", 100)
+//   .attr("y1", 80)
+//   .attr("y2", 20)
+//   .attr("stroke", "black");
+
+// svg
+//   .append("rect")
+//   .attr("x", 50)
+//   .attr("y", 50)
+//   .attr("width", 50)
+//   .attr("height", 80)
+//   .attr("rx", 15)
+//   .attr("ry", 15)
+// .attr("fill", "green");
+
+// TEXT EXAMPLE
+// svg
+//   .append("text")
+//   .text("Hello")
+//   .attr("text-anchor", "start")
+//   .attr("fill", "blue")
+//   .attr("font-size", "28")
+//   .attr("x", 80)
+//   .attr("y", 20);
+// svg
+//   .append("text")
+//   .text("There")
+//   .attr("text-anchor", "middle")
+//   .attr("fill", "blue")
+//   .attr("font-size", "28")
+//   .attr("x", 80)
+//   .attr("y", 50);
+// svg
+//   .append("text")
+//   .text("James")
+//   .attr("stroke", "black")
+//   .attr("text-anchor", "end")
+//   .attr("fill", "blue")
+//   .attr("font-size", "28")
+//   .attr("x", 80)
+//   .attr("y", 80);
